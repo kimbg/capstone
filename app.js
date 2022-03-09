ebc5d99025eeb2be7ea82ea57e6e0dc5157505ea
kimbg <rlaqudrhks6@gmail.com>
2022-03-09 21:59:28 +0900

    ejs partial test

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
-// static files
+// static node modules
 require('dotenv').config();
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = process.env.port_number | 3000;
 
 // router
 const indexRouter = require('./routes/index');
 
-// engine setting
-app.set('views',__dirname + '/views/templates');
+// engine(ejs) setting
+app.set('views',path.join(__dirname,'/views/templates'));
 app.set('view engine','ejs');
+
+//use req.body
+app.use(express.urlencoded({extended:false}));
 
 
 
