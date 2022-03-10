8e87abb14b57a8e678dc241d92fc38a001950571
kimbg <rlaqudrhks6@gmail.com>
2022-03-10 23:50:55 +0900

    register complete

--- routes/login.js
+++ routes/login.js
@@ -3,17 +3,17 @@ const router = express.Router();
 const passport = require('../configure/passport')(router);
 const mySQL_pool = require('../configure/mysql');
 
-let registerCBFunction = (req,res,conn,err,result) => {
+let registerCBFunction = (err,result,req,res,conn) => {
     if(err)
         console.log('registerCBFNC err');
-    else if(!result)
+    else if(!result[0])
     {
         let queryString = `insert into user values (?,?,?)`;
-        conn.query(queryString,[req.body.data.id,req.body.data.pw,req.body.data.age],(err2,result) => {
+        conn.query(queryString,[req.body.id,req.body.pw,req.body.age],(err2,result2) => {
             conn.release();
             if(err2)
                 console.log('registerCBFNC err2')
-            else if(!result)
+            else if(!result2[0])
                 res.send("you register successfully!");            
         })
     }
@@ -41,28 +41,12 @@ router.post('/register',(req,res) => {
 
     mySQL_pool.getConnection((err,conn) => {        
         if(err)
+        {
             console.log("/register conn connect err[1]");
-        
-        let queryString = `select * from user where id = ?`;        
-        conn.query(queryString,[id],(err,result) => {
             conn.release();
-            
-            if(err)
-                console.log("register mysql error");
-            else if(!result[0])
-            {
-                let queryString2 = `insert into user values(?,?,?)`;
-                conn.query(queryString2,[id,pw,age],(err2,result2) => {
-                    if(err2) 
-                        console.log("register mysql error2");                        
-                    else if(!result2[0]) 
-                        res.send("you register successfully!");                        
-                    else 
-                        res.send("there is an error");
-                })
-            }
-            else res.send("already that id has exists");
-        })
+        }        
+        let queryString = `select * from user where id = ?`;        
+        conn.query(queryString,[id],(err,result) => registerCBFunction(err,result,req,res,conn));
     })
 })
 
